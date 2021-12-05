bc275b9caeea3e87f975e3d2301316174145d31f
PoulamiRinki <94235518+PoulamiRinki@users.noreply.github.com>
2021-12-05 18:24:29 +0530

    Add files via upload

---
+++ sum.js
@@ -0,0 +1,15 @@
+let sum = function(param1) {
+    let b = function(param2) {
+        let c = function(param3) {
+
+            let add = param1 + param2 + param3;
+            document.write(add);
+            console.log(add);
+        }
+        return c;
+    }
+    return b;
+}
+    sum(10)(20)(30);
+    
+    
